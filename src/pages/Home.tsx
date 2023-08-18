3e93a6037822e53c4367f0d97d875f77b4a93dfd
Khundrakpam Rajesh <rajesh.lamzing@gmail.com>
2023-08-18 12:28:54 +0530

    Showing manipur-districts.geojson using Leaflet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,20 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { LatLngExpression } from "leaflet";
+import { districtData } from "../assets/manipur-districts";
+import { GeoJSON as GeoJSONType } from "geojson";
 
 function Home() {
   const center = [24.781431, 93.937454] as LatLngExpression;
   const position = [24.781431, 93.937454] as LatLngExpression;
   return (
     <MapContainer center={center} zoom={9} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
       <Marker position={position}>
         <Popup>
           Hey!! You click me. <br />
         </Popup>
       </Marker>
+      <GeoJSON data={districtData as GeoJSONType} />
     </MapContainer>
   );
 }
