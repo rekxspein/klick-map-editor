b04ec478dd3732f932327f2f24ddd8c0551345b8
Khundrakpam Rajesh <rajesh.lamzing@gmail.com>
2023-08-21 16:37:07 +0530

    adding style attribute to GeoJSON

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,15 +7,44 @@ import { GeoJSON as GeoJSONType } from "geojson";
 function Home() {
   const center = [24.781431, 93.937454] as LatLngExpression;
   const position = [24.781431, 93.937454] as LatLngExpression;
+
+  function getColor(Dist_Name: unknown): string | undefined {
+    if (Dist_Name === "Bishnupur") {
+      return "green";
+    } else return "teal";
+  }
+
   return (
-    <MapContainer center={center} zoom={9} scrollWheelZoom={true}>
-      <Marker position={position}>
-        <Popup>
-          Hey!! You click me. <br />
-        </Popup>
-      </Marker>
-      <GeoJSON data={districtData as GeoJSONType} />
-    </MapContainer>
+    <>
+      <MapContainer
+        style={{
+          margin: "30px",
+          height: "80vh",
+          width: "95vh",
+          borderRadius: "1rem",
+        }}
+        center={center}
+        zoom={9}
+        scrollWheelZoom={true}
+      >
+        <Marker position={position}>
+          <Popup>
+            Hey!! You click me. <br />
+          </Popup>
+        </Marker>
+        <GeoJSON
+          data={districtData as GeoJSONType}
+          style={(feature) => ({
+            fillColor: getColor(feature?.properties.Dist_Name), // getColor function
+            weight: 2,
+            opacity: 1,
+            color: "white",
+            dashArray: "3",
+            fillOpacity: 0.7,
+          })}
+        />
+      </MapContainer>
+    </>
   );
 }
 
