1569ca402cd5c0efb730f4b8b6202bfc4f91e5e4
Khundrakpam Rajesh <rajesh.lamzing@gmail.com>
2023-08-22 12:06:59 +0530

    Adding onHover, onClick on each District

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,42 @@
 import { MapContainer, Marker, Popup, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { LatLngExpression } from "leaflet";
+import { LatLngExpression, Layer, LeafletEvent } from "leaflet";
 import { districtData } from "../assets/manipur-districts";
-import { GeoJSON as GeoJSONType } from "geojson";
+import { Feature, GeoJSON as GeoJSONType, Geometry } from "geojson";
 
 function Home() {
   const center = [24.781431, 93.937454] as LatLngExpression;
   const position = [24.781431, 93.937454] as LatLngExpression;
 
-  function getColor(Dist_Name: unknown): string | undefined {
-    if (Dist_Name === "Bishnupur") {
-      return "green";
-    } else return "teal";
-  }
+  const highlightFeature = (e: LeafletEvent) => {
+    const layer = e.target;
+
+    layer.setStyle({
+      weight: 5,
+      dashArray: "3",
+      fillOpacity: 0.7,
+    });
+  };
+
+  const resetHighlight = (e: LeafletEvent) => {
+    const layer = e.target;
+
+    layer.setStyle({
+      weight: 3,
+      dashArray: "",
+      fillOpacity: 0.2,
+    });
+  };
+
+  const onEachFeature = (feature: Feature<Geometry, any>, layer: Layer) => {
+    layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+      // Other interactions...
+    });
+    feature.properties &&
+      layer.bindPopup(`District: ${feature.properties.Dist_Name as unknown}`);
+  };
 
   return (
     <>
@@ -34,14 +58,7 @@ function Home() {
         </Marker>
         <GeoJSON
           data={districtData as GeoJSONType}
-          style={(feature) => ({
-            fillColor: getColor(feature?.properties.Dist_Name), // getColor function
-            weight: 2,
-            opacity: 1,
-            color: "white",
-            dashArray: "3",
-            fillOpacity: 0.7,
-          })}
+          onEachFeature={onEachFeature}
         />
       </MapContainer>
     </>
